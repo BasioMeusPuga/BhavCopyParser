38c351203e68972806e0a2230d61433c836c7d83
BasioMeusPuga <disgruntled.mob@gmail.com>
2017-03-20 03:09:11 +0530

    Supports the NSE as well Removed custom file download Switched to the requests library for downloads

--- BCparse.py
+++ BCparse.py
@@ -6,8 +6,7 @@ import zipfile
 import datetime
 import argparse
 import xlsxwriter
-import urllib.request
-
+import requests
 
 my_cwd = os.getcwd()
 
@@ -37,61 +36,85 @@ class DownloadBhavCopy:
 					print(Colors.RED + 'Date is improper OR not formatted: ' + Colors.GREEN + 'ddmmyy | dd/mm/yy' + Colors.ENDC)
 					exit(1)
 
-	def do_the_dew(self, custom_link):
-		if custom_link is None:
-			# Filename is generated from the date
-			file_date = str(self.bcdate.day).zfill(2) + str(self.bcdate.month).zfill(2) + str(self.bcdate.year)[2:]
-			download_file = 'EQ_ISINCODE_{}.zip'.format(file_date)
-			download_link = 'http://www.bseindia.com/download/BhavCopy/Equity/{}'.format(download_file)
-		else:
-			# Date is taken from the filename
-			download_file = custom_link.rsplit('/', 1)[1]
-			file_date = os.path.splitext(download_file)[0][-6:]
-			download_link = custom_link
+		self.do_the_dew()
 
-		output_file = my_cwd + '/' + download_file
+	def do_the_dew(self):
+		# Generate file name for the BSE bhavcopy
+		file_date = str(self.bcdate.day).zfill(2) + str(self.bcdate.month).zfill(2) + str(self.bcdate.year)[2:]
+		download_file_bse = 'EQ_ISINCODE_{}.zip'.format(file_date)
+		download_link_bse = 'http://www.bseindia.com/download/BhavCopy/Equity/{}'.format(download_file_bse)
 
-		print('Attempting to download: ' + Colors.CYAN + download_link + Colors.ENDC)
+		# Generate file name for the NSE bhavcopy
+		download_file_nse = 'cm{0}{1}{2}bhav.csv.zip'.format(
+			str(self.bcdate.day).zfill(2),
+			self.bcdate.strftime('%B')[:3].upper(),
+			str(self.bcdate.year))
+		download_link_nse = 'https://www.nseindia.com/content/historical/EQUITIES/{2}/{1}/cm{0}{1}{2}bhav.csv.zip'.format(
+			str(self.bcdate.day).zfill(2),
+			self.bcdate.strftime('%B')[:3].upper(),
+			str(self.bcdate.year))
+
+		# Download files to a new directory named according to the date
+		self.final_dir = my_cwd + '/' + str(self.bcdate.day).zfill(2) + '-' + str(self.bcdate.month).zfill(2) + '-' + str(self.bcdate.year)
 		try:
-			urllib.request.urlretrieve(download_link, output_file)
-		except urllib.error.HTTPError:
-			""" No special provisions have been made for a specific http error code:
-			We're working with the assumption that it's 404 """
-			print(Colors.RED + 'Bhavcopy not found' + Colors.ENDC)
-			exit(1)
-		print(Colors.GREEN + 'Download successful' + Colors.ENDC)
+			os.mkdir(self.final_dir)
+		except:
+			pass
 
-		""" Successfully downloaded files are shifted to a new directory named according to
-		the date of the download """
-		self.final_dir = my_cwd + '/' + file_date
-		os.mkdir(self.final_dir)
+		# This will be iterated over repeatedly
+		self.download_files = [
+		{'file': self.final_dir + '/' + download_file_nse, 'link': download_link_nse, 'type': 'nse'},
+		{'file': self.final_dir + '/' + download_file_bse, 'link': download_link_bse, 'type': 'bse'}]
 
-		self.final_file = self.final_dir + '/' + download_file
-		os.rename(output_file, self.final_file)
+		# Proceed to download both bhavcopies
+		for i in self.download_files:
+			print('Attempting to download: ' + Colors.CYAN + i['file'] + Colors.ENDC)
 
-		return True
+			""" I shifted to the requests library because urllib.requests can't seem to
+			do headers easily. Especially for url retrievals """
+			r = requests.get(i['link'], stream=True, headers={'User-agent': 'Mozilla/5.0'})
+			if r.status_code == 200:
+				with open(i['file'], 'wb') as f:
+					for chunk in r.iter_content(chunk_size=1024):
+						if chunk:
+							f.write(chunk)
+				print(Colors.GREEN + 'Download successful' + Colors.ENDC)
+			elif r.status_code == 404:
+				print(Colors.RED + 'Bhavcopy not found' + Colors.ENDC)
+			else:
+				print(Colors.RED + 'HTML error code: ' + str(r.status_code) + Colors.ENDC)
+
+		self.extract_csv()
 
 	def extract_csv(self):
-		bc_zip = zipfile.ZipFile(self.final_file, 'r')
+		for i in self.download_files:
+			bc_zip = zipfile.ZipFile(i['file'], 'r')
 
-		# The name of the csv file is taken from the 0 index of the zip file
-		self.csv_path = self.final_dir + '/' + bc_zip.filelist[0].filename
+			# The name of the csv file is taken from the 0 index of the zip file
+			if i['type'] == 'bse':
+				self.csv_path_bse = self.final_dir + '/' + bc_zip.filelist[0].filename
+			if i['type'] == 'nse':
+				self.csv_path_nse = self.final_dir + '/' + bc_zip.filelist[0].filename
 
-		# It's then extracted to the newly created directory
-		bc_zip.extractall(path=self.final_dir)
-		bc_zip.close()
+			# It's then extracted to the newly created directory
+			bc_zip.extractall(path=self.final_dir)
+			bc_zip.close()
 
-		# Delete the zip file after completion
-		os.remove(self.final_file)
+			# Delete the zip file after completion
+			os.remove(i['file'])
 
 
 class ParseBhavCopy:
-	def __init__(self, csv_path):
+	def __init__(self, csv_path, stock_exchange, file_date):
 		# Open csv derived from the DownloadBhavCopy class
 		# Take relevant path info from the filename
 		self.csv_path = csv_path
 		self.csv_dir = os.path.dirname(csv_path)
 		self.csv_name = os.path.splitext(os.path.basename(csv_path))[0]
+		self.stock_exchange = stock_exchange
+		self.file_date = file_date
+
+		self.parse_csv()
 
 	def parse_csv(self):
 		self.scrip_data = []
@@ -99,17 +122,28 @@ class ParseBhavCopy:
 		with open(self.csv_path) as csvfile:
 			bc_csv = csv.reader(csvfile, delimiter=',')
 			for row in bc_csv:
-				self.scrip_data.append({
-					'scrip_name': row[1].strip(),
-					'scrip_open': row[4],
-					'scrip_high': row[5],
-					'scrip_low': row[6],
-					'scrip_close': row[7]})
+				if self.stock_exchange == 'bse':
+					self.scrip_data.append({
+						'scrip_name': row[1].strip(),
+						'scrip_open': row[4],
+						'scrip_high': row[5],
+						'scrip_low': row[6],
+						'scrip_close': row[7]})
+				elif self.stock_exchange == 'nse':
+					self.scrip_data.append({
+						'scrip_name': row[0].strip(),
+						'scrip_open': row[2],
+						'scrip_high': row[3],
+						'scrip_low': row[4],
+						'scrip_close': row[5]})
 
 		self.write_xlsx()
 
 	def write_xlsx(self):
-		workbook = xlsxwriter.Workbook(self.csv_dir + '/' + self.csv_name[-6:] + '.xlsx')
+		if self.stock_exchange == 'bse':
+			workbook = xlsxwriter.Workbook(self.csv_dir + '/' + '(BSE) ' + self.file_date + '.xlsx')
+		elif self.stock_exchange == 'nse':
+			workbook = xlsxwriter.Workbook(self.csv_dir + '/' + '(NSE) ' + self.file_date + '.xlsx')
 		bold = workbook.add_format({'bold': True})
 
 		def write_to_worksheet(this_worksheet, line_number, col_a, col_b, col_c, col_d, col_e, set_bold):
@@ -191,7 +225,6 @@ class ParseBhavCopy:
 def main():
 	parser = argparse.ArgumentParser(description='Download (today\'s) Bhavcopy.')
 	parser.add_argument('--bcdate', type=str, nargs=1, help='Specify date for Bhavcopy download', metavar='ddmmyy or dd/mm/yy')
-	parser.add_argument('--custom', type=str, nargs=1, help='Link to Bhavcopy', metavar='<url>')
 	args = parser.parse_args()
 
 	if args.bcdate:
@@ -199,17 +232,12 @@ def main():
 	else:
 		get_bhavcopy = DownloadBhavCopy(None)
 
-	if args.custom:
-		download_status = get_bhavcopy.do_the_dew(args.custom[0])
-	else:
-		download_status = get_bhavcopy.do_the_dew(None)
+	file_date = str(get_bhavcopy.bcdate.day).zfill(2) + '-' + str(get_bhavcopy.bcdate.month).zfill(2) + '-' + str(get_bhavcopy.bcdate.year)
 
-	if download_status is True:
-		get_bhavcopy.extract_csv()
-
-		bhavcopy_shenanigans = ParseBhavCopy(get_bhavcopy.csv_path)
-		bhavcopy_shenanigans.parse_csv()
-		print(Colors.GREEN + 'Parsing complete.' + Colors.ENDC)
+	ParseBhavCopy(get_bhavcopy.csv_path_bse, 'bse', file_date)
+	print(Colors.GREEN + 'Parsing complete.' + Colors.ENDC)
+	ParseBhavCopy(get_bhavcopy.csv_path_nse, 'nse', file_date)
+	print(Colors.GREEN + 'Parsing complete.' + Colors.ENDC)
 
 
 if __name__ == '__main__':
